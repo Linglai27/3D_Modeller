903a0bfa305a64119949a1dbdf7c395ca1158490
Linglai Chen <linglai27@yahoo.com>
2022-03-28 13:03:21 -0400

    rendering of snowball

--- node.py
+++ node.py
@@ -3,6 +3,7 @@ from OpenGL.GL import glCallList, glColor3f, glMaterialfv, glMultMatrixf, glPopM
 import numpy
 import color
 from primitive import G_OBJ_SPHERE
+from transformation import scaling, translation
 
 class Node(object):
     def __init__(self):
@@ -30,6 +31,12 @@ class Node(object):
     def render_self(self):
         raise NotImplementedError("The Abstract Node Class doesn't define 'render_self'")
 
+    def translate(self, x, y, z):
+        self.translation_matrix = numpy.dot(self.translation_matrix, translation([x, y, z]))
+
+    def scale(self, s):
+        self.scaling_matrix = numpy.dot(self.scaling_matrix, scaling([s,s,s]))
+
 class Primitive(Node):
     def __init__(self):
         super(Primitive, self).__init__()
@@ -38,6 +45,14 @@ class Primitive(Node):
     def render_self(self):
         glCallList(self.call_list)
 
+class HierarchicalNode(Node):
+    def __init__(self):
+        super(HierarchicalNode, self).__init__()
+        self.child_nodes = []
+
+    def render_self(self):
+        for child in self.child_nodes:
+            child.render()
 
 class Sphere(Primitive):
 
@@ -45,3 +60,21 @@ class Sphere(Primitive):
     def __init__(self):
         super(Sphere, self).__init__()
         self.call_list = G_OBJ_SPHERE
+
+class SnowFigure(HierarchicalNode):
+    def __init__(self):
+        super(SnowFigure, self).__init__()
+        self.child_nodes = [Sphere(), Sphere(), Sphere()]
+        self.child_nodes[0].translate(0, -0.6, 0)
+        self.child_nodes[1].translate(0, 0.1, 0)
+        self.child_nodes[1].scale(0.8)
+        self.child_nodes[2].translate(0, 0.75, 0)
+        self.child_nodes[2].scale(0.7)
+        for child_node in self.child_nodes:
+            child_node.color_index = color.MIN_COLOR
+
+class Cube(Primitive):
+    """ 立方体图元 """
+    def __init__(self):
+        super(Cube, self).__init__()
+        self.call_list = G_OBJ_CUBE
--- primitive.py
+++ primitive.py
@@ -3,7 +3,8 @@ from OpenGL.GL import glBegin, glColor3f, glEnd, glEndList, glLineWidth, glNewLi
 from OpenGL.GLU import gluDeleteQuadric, gluNewQuadric, gluSphere
 
 G_OBJ_SPHERE = 2
-
+G_OBJ_PLANE = 5
+G_OBJ_CUBE = 4
 def make_sphere():
     """ 创建球形的渲染函数列表 """
     glNewList(G_OBJ_SPHERE, GL_COMPILE)
@@ -12,6 +13,88 @@ def make_sphere():
     gluDeleteQuadric(quad)
     glEndList()
 
+def make_plane():
+        glNewList(G_OBJ_PLANE, GL_COMPILE)
+        glBegin(GL_LINES)
+        glColor3f(0, 0, 0)
+        for i in range(41):
+            glVertex3f(-10.0 + 0.5 * i, 0, -10)
+            glVertex3f(-10.0 + 0.5 * i, 0, 10)
+            glVertex3f(-10.0, 0, -10 + 0.5 * i)
+            glVertex3f(10.0, 0, -10 + 0.5 * i)
+
+        # Axes
+        glEnd()
+        glLineWidth(5)
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(5, 0.0, 0.0)
+        glEnd()
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 5, 0.0)
+        glEnd()
+
+        glBegin(GL_LINES)
+        glColor3f(0.5, 0.7, 0.5)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 0.0, 5)
+        glEnd()
+
+        # Draw the Y.
+        glBegin(GL_LINES)
+        glColor3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 5.0, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(-0.5, 6.0, 0.0)
+        glVertex3f(0.0, 5.5, 0.0)
+        glVertex3f(0.5, 6.0, 0.0)
+
+        # Draw the Z.
+        glVertex3f(-0.5, 0.0, 5.0)
+        glVertex3f(0.5, 0.0, 5.0)
+        glVertex3f(0.5, 0.0, 5.0)
+        glVertex3f(-0.5, 0.0, 6.0)
+        glVertex3f(-0.5, 0.0, 6.0)
+        glVertex3f(0.5, 0.0, 6.0)
+
+        # Draw the X.
+        glVertex3f(5.0, 0.0, 0.5)
+        glVertex3f(6.0, 0.0, -0.5)
+        glVertex3f(5.0, 0.0, -0.5)
+        glVertex3f(6.0, 0.0, 0.5)
+
+        glEnd()
+        glLineWidth(1)
+        glEndList()
+
+def make_cube():
+    glNewList(G_OBJ_CUBE, GL_COMPILE)
+    vertices = [((-0.5, -0.5, -0.5), (-0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), (-0.5, 0.5, -0.5)),
+                ((-0.5, -0.5, -0.5), (-0.5, 0.5, -0.5), (0.5, 0.5, -0.5), (0.5, -0.5, -0.5)),
+                ((0.5, -0.5, -0.5), (0.5, 0.5, -0.5), (0.5, 0.5, 0.5), (0.5, -0.5, 0.5)),
+                ((-0.5, -0.5, 0.5), (0.5, -0.5, 0.5), (0.5, 0.5, 0.5), (-0.5, 0.5, 0.5)),
+                ((-0.5, -0.5, 0.5), (-0.5, -0.5, -0.5), (0.5, -0.5, -0.5), (0.5, -0.5, 0.5)),
+                ((-0.5, 0.5, -0.5), (-0.5, 0.5, 0.5), (0.5, 0.5, 0.5), (0.5, 0.5, -0.5))]
+    normals = [(-1.0, 0.0, 0.0), (0.0, 0.0, -1.0), (1.0, 0.0, 0.0), (0.0, 0.0, 1.0), (0.0, -1.0, 0.0), (0.0, 1.0, 0.0)]
+
+    glBegin(GL_QUADS)
+    for i in range(6):
+        glNormal3f(normals[i][0], normals[i][1], normals[i][2])
+        for j in range(4):
+            glVertex3f(vertices[i][j][0], vertices[i][j][1], vertices[i][j][2])
+    glEnd()
+    glEndList()
+
 def init_primitives():
     """ 初始化所有的图元渲染函数列表 """
+    make_plane()
     make_sphere()
+    make_cube()
+
+
